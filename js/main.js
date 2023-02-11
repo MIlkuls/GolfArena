081d996e1e8bf977c0543062d1810b2e87d43511
Klim Gorbachev <klimgorbaccev@gmail.com>
2023-02-12 00:35:57 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
-// $(function() {
-//     let header = $('.header');
-     
-//     $(window).scroll(function() {
-//       if($(this).scrollTop() > 1) {
-//        header.addClass('header_fixed');
-//       } else {
-//        header.removeClass('header_fixed');
-//       }
-//     });
-//    });
-
-//    var hiddenElement = document.getElementById("ab_us");
-//    var btn = document.querySelector('.ab_href');
-   
-//    function handleButtonClick() {
-//       hiddenElement.scrollIntoView({block: "center", inline:"nearest", behavior: "smooth"});
-//    }
-   
-
-//    navigator.connection.downlink;
 
 $(document).ready(function(){
-    $('.burger-menu').click(function(event){
+    $('.burger-menu').click(function(){
       $('.burger-menu , .header-menu').toggleClass('active');
       $('body').toggleClass('lock');
     })
   })
-  $('.menu-list').click(function(event) {
+  $('.menu-list').click(function() {
     $('.burger-menu , .header-menu').removeClass('active');
     $('body').removeClass('lock');
   })
